0960f551bc3d6af66b7a8cefcaba83685a5eb027
Asm Shad (INT) <asm.shad@liz.com>
2025-01-05 14:42:16 +0600

    Menu Button Configuration complete

--- src/Componenets/About/About.jsx
+++ src/Componenets/About/About.jsx
@@ -5,7 +5,7 @@ import pro_pic from "../../assets/pro_pic.jpeg";
 
 const About = () => {
   return (
-    <div className="about">
+    <div id="about" className="about">
       <div className="about-title">
         <h1>About Me</h1>
         <img src={theme_pattern} alt="" />
--- src/Componenets/Hero/Hero.jsx
+++ src/Componenets/Hero/Hero.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Hero.css";
 import pro_img from "../../assets/profile.png";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 export default function Hero() {
   return (
-    <div className="hero">
+    <div id="home" className="hero">
       <img src={pro_img} alt="" />
       <h1>
         <span>I am ASM Shad,</span> MERN Stack developer specialised in REACT.
@@ -14,7 +15,11 @@ export default function Hero() {
         Bangladesh with 3 year of experience.
       </p>
       <div className="hero-action">
-        <div className="hero-connect">Connect with me</div>
+        <div className="hero-connect">
+          <AnchorLink className="anchor-link" offset={50} href="#contact">
+            Connect with me
+          </AnchorLink>
+        </div>
         <div className="hero-resume">My resume</div>
       </div>
     </div>
