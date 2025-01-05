5b14404e92c86abb040a1b17363ac2797dff65bc
Asm Shad (INT) <asm.shad@liz.com>
2025-01-05 14:29:33 +0600

    Footer and NavBar underline added

--- src/Componenets/Footer/Footer.jsx
+++ src/Componenets/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Footer.css";
 import footer_logo from "../../assets/footer_logo.svg";
+import user_icon from "../../assets/user_icon.svg";
 
 const Footer = () => {
   return (
@@ -9,7 +10,28 @@ const Footer = () => {
         <div className="footer-top">
           <div className="footer-top-left">
             <img src={footer_logo} alt="" />
-            <p>I am a MERN Stack developer from, Bangladesh</p>
+            <p>
+              I am a MERN Stack developer from, Bangladesh with 3 years
+              experience in companies.
+            </p>
+          </div>
+          <div className="footer-top-right">
+            <div className="footer-email-input">
+              <img src={user_icon} alt="" />
+              <input type="email" placeholder="Enter Your Email" />
+            </div>
+            <div className="footer-subscribe">Subscribe</div>
+          </div>
+        </div>
+        <hr />
+        <div className="footer-bottom">
+          <p className="footer-bottom-left">
+            &copy; 2025 ASM Shad. All rights reserved.
+          </p>
+          <div className="footer-bottom-right">
+            <p>Terms of Services</p>
+            <p>Privacy Policy</p>
+            <p>Connect With Me</p>
           </div>
         </div>
       </div>
